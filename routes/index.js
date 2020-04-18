1383ce97097508b39d4037eb102a9a502d32a877
jorbegozo <jorbegozo@gmail.com>
2020-04-18 20:47:12 +0200

    Initial version

---
+++ routes/index.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var router = express.Router();
+const bodyParser = require('body-parser');
+router.use(bodyParser.urlencoded({ extended: false }));
+router.use(bodyParser.json());
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://service:zxcvbnm@localhost:27017/conecta";
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+/* Nosotros. */
+router.get('/proyectos', function(req, res, next) {
+  // query mongo
+  res.render('proyectos', { title: 'Proyectos' }); // array : array
+});
+
+/* Servicios. */
+router.get('/voluntarios', function(req, res, next) {
+  res.render('voluntarios', { title: 'Voluntarios' });
+});
+
+/* typeformwebhook */
+router.post('/addresponse', function(req, res, next) {
+	//console.log(JSON.stringify(req.body));
+	MongoClient.connect(url, function(err, db) {
+		  if (err) throw err;
+		  var dbo = db.db("conecta");
+		  var myobj = req.body
+		  dbo.collection("test").insertOne(myobj, function(err, res) {
+			      if (err) throw err;
+			      console.log("1 document inserted");
+			      db.close();
+			    });
+	});
+	  respuesta = {
+		     error: false,
+		     codigo: 200,
+		     mensaje: 'Todo OK'
+		    };
+	  res.send(respuesta)
+});
+
+
+
+
+module.exports = router;
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var HtmlWebpackPugPlugin = require('html-webpack-pug-plugin');
+
+module.exports = {
+    entry: {
+        app: './app.js'
+    },
+    module: {
+        rules: [
+          {
+            test: /\.pug$/,
+            use: [
+              "raw-loader",
+              "pug-html-loader"
+            ]
+          }
+        ],
+        noParse: function (content) {
+            return /express/.test(content);
+        }
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: 'views/index.pug',
+        filename: 'index.html'
+      }),
+      new HtmlWebpackPlugin({
+        template: 'views/proyectos.pug',
+        filename: 'proyectos.html'
+      }),
+      new HtmlWebpackPlugin({
+        template: 'views/voluntarios.pug',
+        filename: 'voluntarios.html'
+      }),
+      new HtmlWebpackPlugin({
+        template: 'views/contacto.pug',
+        filename: 'contacto.html'
+      })
+    ],
+    mode: 'production',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    }
+};
